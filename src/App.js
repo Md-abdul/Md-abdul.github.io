3f975ba7bf6edd3fb9b50a87b7b71013b3594036
Md-abdul <mdabdulq62@gmail.com>
2023-10-30 19:32:34 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import NavMenu from "./components/NavMenu";
 
 function App() {
   return (
     <div className="App">
-     hello form abdul
+      <NavMenu />
     </div>
   );
 }
