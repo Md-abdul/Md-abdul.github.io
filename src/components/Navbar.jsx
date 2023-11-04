01ded6bd8ea0a983c7f48524bfc4e24d5d0a557f
Md-abdul <mdabdulq62@gmail.com>
2023-11-04 11:18:15 +0530

    chekc test case

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,71 +1,4 @@
-// import { useRef } from "react";
-// import { FaBars, FaTimes } from "react-icons/fa";
-// import "../style/Nav.css";
-// import { Button, Link, Text } from "@chakra-ui/react";
-// import Md_Abdul_Qadir_Resume from "../assets/Md_Abdul_Qadir_Resume.pdf.pdf"
-// import { AiOutlineDownload } from "react-icons/ai";
-// function Navbar() {
-//   const navRef = useRef();
-//   const Logo = "< Abdul />";
-//   const showNavbar = () => {
-//     navRef.current.classList.toggle("responsive_nav");
-//   };
-
-//   return (
-//     <header id="nav-menu">
-//       <div>
-//         <h1>{Logo}</h1>
-//       </div>
-//       <nav id="nav-header" ref={navRef}>
-//         {/* <div> */}
-//         <Link className="nav-link home" href="/home">
-//           Home
-//         </Link>
-//         <Link className="nav-link about" href="#about">
-//           About
-//         </Link>
-//         <Link className="nav-link skills" href="#skills">
-//           Skills
-//         </Link>
-//         <Link className="nav-link projects" href="#projects">
-//           Projects
-//         </Link>
-//         <Link className="nav-link contact" href="#contact">
-//           Contact
-//         </Link>
-//         <Button
-//           colorScheme={"red"}
-//           id="resume-button-1"
-//           className="nav-link resume"
-//           onClick={() => {
-//             window.open(
-//               "https://drive.google.com/file/d/1kPQzRRLng5ZdCBcira_ARtw0-eWzXYdL/view?usp=sharing",
-//               "_blank"
-//             );
-//           }}
-          
-//         >
-//           <a href={Md_Abdul_Qadir_Resume} target="_blank" download='Md_Abdul_Qadir_Resume.pdf'>Resume</a>
-//         </Button>
-//         {/* </div> */}
-
-//         <button className="nav-btn nav-close-btn" onClick={showNavbar}>
-//           <FaTimes />
-//         </button>
-//       </nav>
-
-//       <button className="nav-btn" onClick={showNavbar}>
-//         <FaBars />
-//       </button>
-//     </header>
-//   );
-// }
-
-// export default Navbar;
-
-
-
-import { useRef } from "react";
+import { useRef, useState } from "react";
 import { FaBars, FaTimes } from "react-icons/fa";
 import "../style/Nav.css";
 import { Button, Link, Text } from "@chakra-ui/react";
@@ -74,6 +7,7 @@ import { AiOutlineDownload } from "react-icons/ai";
 
 function Navbar() {
   const navRef = useRef();
+  const [Name, setName] = useState("Home");
   const Logo = "< Abdul />";
   const showNavbar = () => {
     navRef.current.classList.toggle("responsive_nav");
@@ -92,20 +26,40 @@ function Navbar() {
         <h1>{Logo}</h1>
       </div>
       <nav id="nav-header" ref={navRef}>
-        <Link className="nav-link home" href="/home">
-          Home
+        <Link
+          onClick={() => setName("Home")}
+          className="nav-link home"
+          href="#home"
+        >
+          home
         </Link>
-        <Link className="nav-link about" href="#about">
-          About
+        <Link
+          onClick={() => setName("About")}
+          className="nav-link about"
+          href="#about"
+        >
+          about
         </Link>
-        <Link className="nav-link skills" href="#skills">
-          Skills
+        <Link
+          onClick={() => setName("Skills")}
+          className="nav-link skills"
+          href="#skills"
+        >
+          skills
         </Link>
-        <Link className="nav-link projects" href="#projects">
-          Projects
+        <Link
+          onClick={() => setName("Projects")}
+          className="nav-link projects"
+          href="#projects"
+        >
+          projects
         </Link>
-        <Link className="nav-link contact" href="#contact">
-          Contact
+        <Link
+          onClick={() => setName("Contact")}
+          className="nav-link contact"
+          href="#contact"
+        >
+          contact
         </Link>
         <Button
           colorScheme={"red"}
--- src/components/Skill.jsx
+++ src/components/Skill.jsx
@@ -1,83 +1,78 @@
 import React from "react";
-import { Box, Flex, Image, Text } from "@chakra-ui/react";
+import { Box, Heading, Flex, Image, Text } from "@chakra-ui/react";
+import htmlImg from "../assets/html.png";
+import cssImg from "../assets/css-3.svg";
+import javascriptImg from "../assets/javascript-1.svg";
+import reactImg from "../assets/react-2.svg";
+import reduxImg from "../assets/redux.svg";
+import mongodbImg from "../assets/mongodb-icon-1.svg";
+import nodejsImg from "../assets/nodejs-2.svg";
+import mongooseImg from "../assets/mongoose-1.svg";
+import chakraUIImg from "../assets/chakraui_logo.png";
+import gitImg from "../assets/github.svg";
+import postmanImg from "../assets/postman.svg";
 
-const skillsData = [
-  {
-    id: 1,
-    name: "HTML",
-    imgSrc: "../assets/html-1.svg",
-  },
-  {
-    id: 2,
-    name: "CSS",
-    imgSrc: "../assets/css-3.svg",
-  },
-  {
-    id: 3,
-    name: "JavaScript",
-    imgSrc: "../assets/javascript-1.svg",
-  },
-  {
-    id: 4,
-    name: "React",
-    imgSrc: "../assets/react-2.svg",
-  },
-  {
-    id: 5,
-    name: "Redux",
-    imgSrc: "../assets/redux.svg",
-  },
-  {
-    id: 6,
-    name: "MongoDB",
-    imgSrc: "assets/img/mongodb-icon-1.svg",
-  },
-  {
-    id: 7,
-    name: "Node.js",
-    imgSrc: "assets/img/nodejs-2.svg",
-  },
-  {
-    id: 8,
-    name: "Mongoose",
-    imgSrc: "assets/img/mongoose-1.svg",
-  },
-  {
-    id: 9,
-    name: "Chakra UI",
-    imgSrc: "assets/img/chakraui_logo.png",
-  },
-  {
-    id: 10,
-    name: "GIT",
-    imgSrc: "assets/img/github.svg",
-  },
-  {
-    id: 11,
-    name: "Postman",
-    imgSrc: "assets/img/postman.svg",
-  },
-];
-
-const Skills = () => {
+const Skill = () => {
   return (
-    <section id="skills">
-      <Text fontSize="4xl" fontWeight="600" textAlign="center" id="skills1" className="about section" mt="200px">
+    <Box
+      id="skills"
+      p="15"
+      w={{ base: "100%", lg: "80%", md: "50%" }}
+      m={"auto"}
+      style={{ border: "1px solid" }}
+      mt={59}
+    >
+      <Heading
+        id="skills1"
+        size="md"
+        mb="4"
+        fontSize={{ base: "3xl", md: "4xl" }}
+        fontWeight="600"
+        textAlign="center"
+      >
         Skills
-      </Text>
-
-      <Flex wrap="wrap" justify="center" align="center" mt="20px">
-        {skillsData.map((skill) => (
-          <Box key={skill.id} className="skills-card" textAlign="center" width={{ base: "50%", sm: "30%", md: "20%" }} mb="20px">
-            <Image src={skill.imgSrc} alt={skill.name} className="skills-card-img" />
-            <Text className="skills-card-name" mt="10px">
+      </Heading>
+      <Flex flexWrap="wrap" justifyContent="space-around" gap={-20}>
+        {skillsData.map((skill, index) => (
+          <Box
+            key={index}
+            className="skills-card"
+            margin={"auto"}
+            width={["40%", "25%", "15%"]} // Adjust the width for different screen sizes
+            p="4"
+            style={{ border: "1px solid" }}
+            mt={5}
+            borderRadius={"50"}
+          >
+            <Image
+              className="skills-card-img"
+              src={skill.icon}
+              alt={`Skill ${index + 1}`}
+              mx="auto"
+              boxSize="100px"
+            />
+            <Text className="skills-card-name" textAlign="center" mt="2">
               {skill.name}
             </Text>
           </Box>
         ))}
       </Flex>
-    </section>
+    </Box>
   );
 };
 
-export default Skills;
+export default Skill;
+
+const skillsData = [
+  { name: "HTML", icon: htmlImg },
+  { name: "CSS", icon: cssImg },
+  { name: "JavaScript", icon: javascriptImg },
+  { name: "React", icon: reactImg },
+  { name: "Redux", icon: reduxImg },
+  { name: "Mongo DB", icon: mongodbImg },
+  { name: "Node.js", icon: nodejsImg },
+  { name: "Mongoose", icon: mongooseImg },
+  { name: "Chakra UI", icon: chakraUIImg },
+  { name: "GIT", icon: gitImg },
+  { name: "Postman", icon: postmanImg },
+];
